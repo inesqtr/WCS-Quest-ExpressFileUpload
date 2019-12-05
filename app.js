d7b7ee022b267af15c90600afd25fcc7390207ee
Diogo Rodrigues <diogofgr@gmail.com>
2019-12-05 17:26:56 +0000

    add mysql config

--- app.js
+++ app.js
@@ -1,14 +1,14 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
-var sassMiddleware = require('node-sass-middleware');
+const createError = require('http-errors');
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
+const sassMiddleware = require('node-sass-middleware');
 
-var indexRouter = require('./routes/index');
-var postsRouter = require('./routes/posts');
+const indexRouter = require('./routes/index');
+const postsRouter = require('./routes/posts');
 
-var app = express();
+const app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
